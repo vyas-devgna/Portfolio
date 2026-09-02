f78d12aa7121b9831babdfa92e1778207581728c
Vyas Devgna <141345367+vyas-devgna@users.noreply.github.com>
2026-09-03 04:06:00 +0530

    chore: remove obsolete server dependencies and tracked build clutter

--- test_project.js
+++
@@ -1,9 +0,0 @@
-const assert = require('node:assert/strict');
-const fs = require('node:fs');
-
-const html = fs.readFileSync('index.html', 'utf8');
-assert.match(html, /<h3 class="project-title">EzBoard<\/h3>/);
-assert.match(html, /https:\/\/ezboard\.vyasdevgna\.online/);
-assert.match(html, /https:\/\/github\.com\/vyas-devgna\/ezboard/);
-assert.doesNotMatch(html, /project-showcase/);
-console.log('EzBoard portfolio card check passed');
